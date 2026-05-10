1455f57d37e44d6523fbe6567e9ec14b8ebf437f
ooiws <ooiwensong@gmail.com>
2026-05-10 08:18:47 +0800

    Add keep-alive server

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"net/http"
 	"os"
 	"os/signal"
 
@@ -10,6 +12,25 @@ import (
 
 func main() {
 	godotenv.Load()
+
+	// Keep-alive server to prevent Render deployment from sleeping
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, "OK")
+	})
+
+	go func() {
+		fmt.Printf("Starting keep-alive server on port %s...\n", port)
+		if err := http.ListenAndServe(":"+port, nil); err != nil {
+			fmt.Printf("Keep-alive server error: %v\n", err)
+		}
+	}()
+
+	// Telegram bot
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer cancel()
 
